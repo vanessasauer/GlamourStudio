bf2c0df116fb7ec67ac6fd9360e78341c34bb058
vanessa.sauer <vanessasauer044@gmail.com>
2026-09-04 19:32:49 -0300

    At com front

--- src/main/java/com/example/glamourstudio/repository/ServicoRepository.java
+++ src/main/java/com/example/glamourstudio/repository/ServicoRepository.java
@@ -10,6 +10,5 @@ import java.util.Optional;
 
 @Repository
 public interface ServicoRepository extends JpaRepository<Servico, Long> {
-
-    Optional<List<Servico>> findByStatusNot(EnumStatusServico statusServico);
+    
 }
